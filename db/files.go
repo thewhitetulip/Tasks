632e811b75e14d30847e49f4c68ffc3d27e056fc
Christian Huff <Christian.Huff.ch@gmail.com>
2016-05-17 09:42:39 +0100

    Fix error occurring when attaching a file to a Task
    Extend the SQL storing the information to a file by username and creation date.

--- db/files.go
+++ db/files.go
@@ -12,7 +12,11 @@ import (
 // AddFile is used to add the md5 of a file name which is uploaded to our application
 // this will enable us to randomize the URL without worrying about the file names
 func AddFile(fileName, token, username string) error {
-	err := taskQuery("insert into files values(?,?,?,datetime())", fileName, token, username)
+	userID, err := GetUserID(username)
+	if err != nil {
+		return err
+	}
+	err = taskQuery("insert into files values(?,?,?,datetime())", fileName, token, userID)
 	return err
 }
 
