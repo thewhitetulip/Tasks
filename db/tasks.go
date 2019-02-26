15ed05d39f1f7319130ebdc5547434831dbc0d55
nisakov <niki-isak@yandex.ru>
2019-02-27 02:40:23 +0300

    Fix query for getting task from database.

--- db/tasks.go
+++ db/tasks.go
@@ -167,9 +167,9 @@ func GetTaskByID(username string, id int) (types.Context, error) {
 	var tasks []types.Task
 	var task types.Task
 
-	getTaskSQL := "select t.id, t.title, t.content, t.priority, t.hide, c.name from task t join user u left outer join category c  where c.id = t.cat_id and t.user_id=u.id and t.id=? and u.username=? union select t.id, t.title, t.content, t.priority, t.hide, 'UNCATEGORIZED' from task t join user u where t.user_id=u.id and t.cat_id=0 ;"
+	getTaskSQL := "select t.id, t.title, t.content, t.priority, t.hide, c.name from task t join user u left outer join category c where c.id = t.cat_id and t.user_id=u.id and t.id=? and u.username=? union select t.id, t.title, t.content, t.priority, t.hide, 'UNCATEGORIZED' from task t join user u where t.user_id=u.id and t.cat_id=0 and t.id=? and u.username=?;"
 
-	rows := database.query(getTaskSQL, id, username)
+	rows := database.query(getTaskSQL, id, username, id, username)
 	defer rows.Close()
 	if rows.Next() {
 		err := rows.Scan(&task.Id, &task.Title, &task.Content, &task.Priority, &task.IsHidden, &task.Category)
